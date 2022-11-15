3a8318fbec35947ea6c405410a48a2c4e10a8d0c
Daniel Legan <legan.daniel@gmail.com>
2022-11-15 12:25:53 -0600

    create database

--- database.sql
+++ database.sql
@@ -8,3 +8,35 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+-- Database Used
+DROP TABLE IF EXISTS "users";
+DROP TABLE IF EXISTS "games";
+DROP TABLE IF EXISTS "connections";
+
+CREATE TABLE "users" (
+	"id" serial PRIMARY KEY,
+	"username" VARCHAR (80) UNIQUE NOT NULL,
+	"password" VARCHAR (1000) NOT NULL,
+	"address" varchar(255) NOT NULL,
+	"lat" DECIMAL(10) NOT NULL,
+	"lng" DECIMAL(10) NOT NULL,
+	"game_id" int NOT NULL REFERENCES games,
+	"noob_or_master" varchar(6) NOT NULL
+	);
+	
+CREATE TABLE "games" (
+	"id" serial PRIMARY KEY,
+	"title" VARCHAR(255) NOT NULL
+	);
+	
+CREATE TABLE "connections" (
+	"id" serial PRIMARY KEY,
+	"noob_id" int REFERENCES users,
+	"master_id" int REFERENCES users,
+	"noob_message" VARCHAR(255) NOT NULL,
+	"master_message" VARCHAR(255) DEFAULT NULL,
+	"status" VARCHAR(9) DEFAULT 'pending',
+	"master_rating" int DEFAULT NULL,
+	"noob_message_counter" int DEFAULT 1
+	);
