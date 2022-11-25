b08c5d227a5c57ec0263800e5d4aab8cb6e255eb
Daniel Legan <legan.daniel@gmail.com>
2022-11-25 16:52:12 -0600

    stopping point

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,7 +5,7 @@ import {
   Route,
   Switch,
 } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import Nav from '../Nav/Nav';
 import Footer from '../Footer/Footer';
 import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
@@ -14,14 +14,12 @@ import LandingPage from '../LandingPage/LandingPage';
 import Map from '../Map/Map';
 import RequestsPage from '../RequestsPage/RequestsPage';
 import InvitesPage from '../InvitesPage/InvitesPage';
-import EditUser from '../EditUser/EditUser';
+import { ParallaxProvider } from 'react-scroll-parallax';
 
 import './App.css';
 
 function App() {
   const dispatch = useDispatch();
-
-  const user = useSelector(store => store.user);
 
   useEffect(() => {
     dispatch({ type: 'FETCH_USER' });
@@ -30,115 +28,62 @@ function App() {
   return (
     <Router>
       <div>
-        <Nav />
-        <Switch>
-          {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
-          <Redirect exact from="/" to="/home" />
 
-          {/* Visiting localhost:3000/about will show the about page. */}
-          <Route
-            // shows AboutPage at all times (logged in or not)
-            exact
-            path="/about"
-          >
-            <AboutPage />
-          </Route>
+          <Nav />
+          <Switch>
 
-          {/* For protected routes, the view could show one of several things on the same route.
+            <Redirect exact from="/" to="/home" />
+
+            <Route
+              exact
+              path="/about"
+            >
+              <AboutPage />
+            </Route>
+
+            {/* For protected routes, the view could show one of several things on the same route.
             Visiting localhost:3000/user will show the UserPage if the user is logged in.
             If the user is not logged in, the ProtectedRoute will show the LoginPage (component).
             Even though it seems like they are different pages, the user is always on localhost:3000/user */}
-          
-          {/* <ProtectedRoute
-            // logged in shows UserPage else shows LoginPage
-            exact
-            path="/user"
-          >
 
-          </ProtectedRoute> */}
+            <ProtectedRoute
 
-          {/* <ProtectedRoute
+              exact
+              path="/map"
+            >
+              <Map />
+            </ProtectedRoute>
 
-            exact
-            path="/info"
-          >
-            <LandingPage />
-          </ProtectedRoute> */}
+            <ProtectedRoute
 
-          <ProtectedRoute
+              exact
+              path="/requests"
+            >
+              <RequestsPage />
+            </ProtectedRoute>
 
-            exact
-            path="/map"
-          >
-            <Map />
-          </ProtectedRoute>
+            <ProtectedRoute
 
-          <ProtectedRoute
+              exact
+              path="/invites"
+            >
+              <InvitesPage />
+            </ProtectedRoute>
 
-            exact
-            path="/requests"
-          >
-            <RequestsPage />
-          </ProtectedRoute>
-
-          <ProtectedRoute
-
-            exact
-            path="/invites"
-          >
-            <InvitesPage />
-          </ProtectedRoute>
-
-          {/* <Route
-            exact
-            path="/login"
-          >
-            {user.id ?
-              // If the user is already logged in, 
-              // redirect to the /user page
-              <Redirect to="/home" />
-              :
-              // Otherwise, show the login page
-              <LoginPage />
-            }
-          </Route> */}
-
-          {/* <Route
-            exact
-            path="/registration"
-          >
-            {user.id ?
-              // If the user is already logged in, 
-              // redirect them to the /user page
-              <Redirect to="/user" />
-              :
-              // Otherwise, show the registration page
-              <RegisterPage />
-            }
-          </Route> */}
-
-          <Route
-            exact
-            path="/home"
-          >
-            {/* {user.id ?
-              // If the user is already logged in, 
-              // redirect them to the /user page
-              <Redirect to="/user" />
-              :
-              // Otherwise, show the Landing page
+            <Route
+              exact
+              path="/home"
+            >
               <LandingPage />
-            } */}
-            
-            <LandingPage />
-          </Route>
+            </Route>
 
-          {/* If none of the other routes matched, we will show a 404. */}
-          <Route>
-            <h1>404</h1>
-          </Route>
-        </Switch>
-        <Footer />
+            {/* If none of the other routes matched, we will show a 404. */}
+            <Route>
+              <h1>404</h1>
+            </Route>
+          </Switch>
+          <Footer />
+
       </div>
     </Router>
   );
--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,52 +1,24 @@
 import React, { useState } from 'react';
+// import { Parallax } from 'react-scroll-parallax';
+import { Parallax } from 'react-parallax';
 import './LandingPage.css';
+import gamers from '../images/gamers.jpg'
 
 // CUSTOM COMPONENTS
 
 function LandingPage() {
-  const [heading, setHeading] = useState('Welcome');
 
   return (
     <>
-      <div className="container">
-        <h2>{heading}</h2>
-
-        <div className="grid">
-          <div className="grid-col grid-col_8">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-              id felis metus. Vestibulum et pulvinar tortor. Morbi pharetra lacus
-              ut ex molestie blandit. Etiam et turpis sit amet risus mollis
-              interdum. Suspendisse et justo vitae metus bibendum fringilla sed
-              sed justo. Aliquam sollicitudin dapibus lectus, vitae consequat odio
-              elementum eget. Praesent efficitur eros vitae nunc interdum, eu
-              interdum justo facilisis. Sed pulvinar nulla ac dignissim efficitur.
-              Quisque eget eros metus. Vestibulum bibendum fringilla nibh a
-              luctus. Duis a sapien metus.
-            </p>
-
-            <p>
-              Praesent consectetur orci dui, id elementum eros facilisis id. Sed
-              id dolor in augue porttitor faucibus eget sit amet ante. Nunc
-              consectetur placerat pharetra. Aenean gravida ex ut erat commodo, ut
-              finibus metus facilisis. Nullam eget lectus non urna rhoncus
-              accumsan quis id massa. Curabitur sit amet dolor nisl. Proin
-              euismod, augue at condimentum rhoncus, massa lorem semper lacus, sed
-              lobortis augue mi vel felis. Duis ultrices sapien at est convallis
-              congue.
-            </p>
-
-            <p>
-              Fusce porta diam ac tortor elementum, ut imperdiet metus volutpat.
-              Suspendisse posuere dapibus maximus. Aliquam vitae felis libero. In
-              vehicula sapien at semper ultrices. Vivamus sed feugiat libero. Sed
-              sagittis neque id diam euismod, ut egestas felis ultricies. Nullam
-              non fermentum mauris. Sed in enim ac turpis faucibus pretium in sit
-              amet nisi.
-            </p>
+          <div>
+            <Parallax strength={800} bgImage={gamers}>
+              <div className='content'>
+                <div className='text-content'>
+                  Normal Parallax
+                </div>
+              </div>
+            </Parallax>
           </div>
-        </div>
-      </div>
     </>
   );
 }
